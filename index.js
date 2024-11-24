93ad8fbb8b8c3ad122948452becbd75720388413
mrrahulkrr <rahulkrgupta18032003@gmail.com>
2024-11-24 21:17:13 +0530

    correcting_index.js

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ app.use(express.json())
 app.set('io', io);
 
 
-app.use('api/users', userRoutes);
-app.use('api/posts', postRoutes);
-app.use('api/comments', commentRoutes);
-app.use('api/chats', chatRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/comments', commentRoutes);
+app.use('/api/chats', chatRoutes);
 
 const PORT = process.env.PORT || 5000;
 httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
